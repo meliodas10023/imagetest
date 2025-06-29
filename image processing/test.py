ba5f36762c0a40abba5a0a55149263fb7cc010bc
xsl <xslxsl545@gmail.com>
2025-06-29 21:58:40 +0800

    hot-fix commit2

--- image processing/test.py
+++ image processing/test.py
@@ -7,6 +7,12 @@ image = cv2.imread('images/ts.jpg')  # 替换为你的图像路径
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+
+cv2.imshow("blurred", blurred)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
 
 
 
